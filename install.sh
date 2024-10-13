34e44efc61b32adacf17828bf2b475e4a9f3eb5f
Sorkand <96412234+sorkand1@users.noreply.github.com>
2024-10-13 14:03:41 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,12 @@ source <(curl -s https://raw.githubusercontent.com/R1M-NODES/utils/master/common
 
 printLogo
 
+if ! command -v docker &> /dev/null; then
 printGreen "Install docker and docker compose"
 bash <(curl -s https://raw.githubusercontent.com/R1M-NODES/utils/master/docker-install.sh)
+else
+    printGreen "Docker is already installed. Skipping installation."
+fi
 
 git clone https://github.com/Uniswap/unichain-node
 
